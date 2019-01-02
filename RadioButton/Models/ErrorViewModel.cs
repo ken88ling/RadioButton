955131573c7640d52d5840eab4768a843abed757
Ken Ling <ken88ling@gmail.com>
2019-01-02 10:04:23 +0800

    Add project files.

---
+++ RadioButton/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RadioButton.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
